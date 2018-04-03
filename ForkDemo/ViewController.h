de9615d5349c46faaec5c78d7fd26aa82f034dcc
yang_shuai <ys_yumicha@163.com>
2018-04-03 14:28:01 +0800

    first commit

---
+++ ForkDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ForkDemo
+//
+//  Created by ys on 2018/4/3.
+//  Copyright © 2018年 Creative Knowledge Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ForkDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ForkDemo
+//
+//  Created by ys on 2018/4/3.
+//  Copyright © 2018年 Creative Knowledge Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
